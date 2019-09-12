fcd33c2939202749753198c164bf1fd308168396
dpsoccerdude101 <37779084+dpsoccerdude101@users.noreply.github.com>
2019-09-12 13:58:05 -0400

    Add files via upload

---
+++ Lab03/AbstractShape.java
@@ -0,0 +1,15 @@
+/**
+ * Abstract class AbstractShape - write a description of the class here
+ *
+ * @author (Kurt Mueller & Dennis Pavlyuk)
+ * @version (9/12/2019)
+ */
+public abstract class AbstractShape implements TwoDShape, ThreeDShape
+{
+    protected static double numShapesCreated;
+    public void incrementNumShapesCreated(){numShapesCreated++;}
+    public double getNumShapesCreated() {return numShapesCreated;}
+    public abstract double area();
+    public abstract double perimeter();
+    public abstract double volume();
+}
---
+++ Lab03/ThreeDShape.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of interface ThreeDSHape here.
+ *
+ * @author (Kurt Mueller & Dennis Pavlyuk)
+ * @version (9/12/2019)
+ */
+public interface ThreeDShape extends Shape
+{
+    public double volume();
+}
