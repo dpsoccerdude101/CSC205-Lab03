9f1e802ceb74d8eb68ed061e3218b22e0065725a
dpsoccerdude101 <dennispavlyuk@gmail.com>
2019-09-20 12:06:51 -0400

    The other half of the lab

---
+++ Lab03b/TestingAndComplexity/IntSortSearchTester.java
@@ -0,0 +1,10 @@
+package TestingAndComplexity;
+public class IntSortSearchTester {
+    public static void linearSearchTester() – This hard-codes the test data for linearSearch method
+    
+    public static void binarySearchTester() – This hard-codes the test data for binarySearch method
+    
+    public static void sortTester() – This hard-codes the test data for selectionSort method
+    
+    public static void main(String[] args) method which just calls all the tester methods, using a statement like: IntSortSearch.linearSearchTester();
+}
